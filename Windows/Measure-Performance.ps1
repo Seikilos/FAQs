97e5213bfef86380745b594d98e97f53b1b8e640
Seikilos <Seikilos@users.noreply.github.com>
2022-08-19 15:53:34 +0200

    Added ability to measure performance for windows systems

---
+++ Windows/Measure-Performance.ps1
@@ -0,0 +1,31 @@
+$item = Get-ChildItem "$env:SystemRoot\Performance\WinSat\DataStore" -Filter "*Formal*" | Sort-Object -Descending {$_.CreationTime} | Select-Object -First 1
+
+[xml]$xml = Get-Content $item.FullName
+
+Write-Output "SPR"
+Write-Output $xml.WinSAT.WinSPR
+
+Write-Output "==============================="
+
+
+function print($depth, $node)
+{
+    $str = ("`t" * $depth) + $node.Name
+   
+    if($node.ChildNodes.Count -eq 1 -and $node.ChildNodes[0].Name -eq "#text")
+    {
+        $str += (": "+ $node.'#text'.Trim()) + " " + ($node.Attributes | Select-Object -ExpandProperty Value)
+    }
+    Write-Output $str
+
+    if ($node.ChildNodes[0].Name -eq "#text")
+    {
+        return
+    }
+
+    $node.ChildNodes | ForEach-Object {
+        print ($depth +1) $_
+    }
+}
+
+print 0 $xml.WinSAT.Metrics
